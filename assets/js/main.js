aefc208429ec5514c7f67bb873169893137074e8
Ivan Buljovcic <ivan.buljovcic@diwanee.com>
2016-06-21 10:49:35 +0200

    Feat: Slick slider script setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
 var slickSliderHP = require('./src/slickHome');
 
-slickSliderHP();
+slickSliderHP('.hpSlider', '.hpSlider__dotsContainer', '.hpSlider__tag', '.hpSlider__title', '.hpSlider__slideTag', '.hpSlider__slideTitle');
--- assets/js/src/slickHome.js
+++ assets/js/src/slickHome.js
@@ -1,13 +1,33 @@
 var $     = require('jquery');
 var slick = require('slick-carousel');
 
-module.exports = function () {
-    $('.hpSlider').slick({
+module.exports = function (slideElement, dotsContainer, visibleTag, visibleTitle, slideTag, slideTitle) {
+    var $slideElement = $(slideElement);
+    var $slideTag     = $(visibleTag);
+    var $slideTitle   = $(visibleTitle);
+
+    $slideElement.on('init afterChange', setText);
+
+    $slideElement.slick({
         slidesToShow  : 1,
         slidesToScroll: 1,
         dots          : true,
+        dotsClass     : dotsContainer,
         infinite      : true,
         speed         : 300,
         centerMode    : true
     });
+
+
+    //------------PLACE ACTIVE ITEM TEXT INTO STATIC TEXT WRAPPER
+    function setText () {
+        var $activeSlide = $slideElement.find('.slick-active');
+
+        var activeTag    = $activeSlide.data('tag');
+        var activeTitle  = $activeSlide.data('title');
+        var activeHref   = $activeSlide.data('href');
+
+        $slideTag.text(activeTag);
+        $slideTitle.text(activeTitle).attr('href', activeHref);
+    }
 }
