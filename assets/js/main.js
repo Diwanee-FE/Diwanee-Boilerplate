29b8b7a5355114af035fc320a0dee515edfe90f7
Justina Valda Rajic <justina.valdarajic@diwanee.com>
2016-06-27 15:33:24 +0200

    sticky element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 var slickSliderHP = require('./src/slickHome');
+var stickySocial  = require('./src/stickySocial');
+
+
 
 slickSliderHP('.hpSlider', '.hpSlider__dotsContainer', '.hpSlider__tag', '.hpSlider__title', '.hpSlider__slideTag', '.hpSlider__slideTitle');
+
+stickySocial('.sticky-social', 10); //#1 argument: sticky element; #2 distance from top when scrolling down
