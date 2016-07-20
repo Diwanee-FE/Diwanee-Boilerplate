8dfd5271e0e5f5a7d1ccbba38cd33a71fab10f2a
Justina Valda Rajic <justina.valdarajic@diwanee.com>
2016-07-20 12:17:55 +0200

    search transition

--- assets/js/src/search.js
+++ assets/js/src/search.js
@@ -9,12 +9,10 @@ module.exports = function (searchContent, searchButton, openClass) {
     var $searchContent = $(searchContent);
     var $searchButton  = $(searchButton);
 
-    var $searchButtonW = $searchButton.outerWidth();
-
-    var $searchContentRight = $searchContent.css('right', $searchButtonW);
+    var searchButtonW = $searchButton.outerWidth() - 2; // overlaping border therefore minus two    
 
     $searchButton.on('click', function() {
         $searchContent.toggleClass('open');
-        console.log('ksdafhjasdfk');
+        $searchContent.css('right', searchButtonW)
     });
 }
