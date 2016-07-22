271318a09fd2e39d41148ea9b68130977f0fdf37
Justina Valda Rajic <justina.valdarajic@diwanee.com>
2016-07-22 12:14:48 +0200

    bug removed

--- assets/js/src/sticky.js
+++ assets/js/src/sticky.js
@@ -28,7 +28,7 @@ module.exports = function (sticky, distanceFromTop, right) {
     * On Window load, get the dimensions of the $sticky, $parent and offsets
     */
     $window.on('load', function () {
-        stickyHeight    = $sticky.outerHeight();
+        stickyHeight    = $sticky.outerHeight(true);
         stickyWidth     = $sticky.outerWidth();
 
         parentWidth     = $stickyParent.outerWidth();
@@ -38,22 +38,40 @@ module.exports = function (sticky, distanceFromTop, right) {
         parentRightEdge = parentLeftEdge + parentWidth;
 
         bottomLimit     = parentOffsetTop + parentHeight;
+
+        setInitialStickyPosition();
+
+        $window.on('scroll touchmove', checkPosition);
     });
 
-    //FN SCROLLITSTICKY()
-    function scrollItSticky () {
+    //Fn setInitialStickyPosition ()
+    function setInitialStickyPosition() {
+        if (!right) {
+           $sticky.css('left', -stickyWidth)
+       } else {
+           $sticky.css('right', -stickyWidth)
+       }
+    }
+    //Fn scrollitsticky ()
+    function checkPosition () {
         wST = $window.scrollTop();
 
-        if (wST >= parentOffsetTop - distanceFromTop && wST <= bottomLimit - stickyHeight - distanceFromTop) { // Scroll position is within the borders of the sticky parent
+        // Scroll position is within the borders of the sticky parent
+        const firstCondition = wST >= parentOffsetTop - distanceFromTop && wST <= bottomLimit - stickyHeight - distanceFromTop - 1;
+
+        // Scroll position is on the bottom of the sticky parent
+        const secondCondition = wST > bottomLimit - stickyHeight - distanceFromTop - 1;
+
+        if (firstCondition) {
             if (!right) {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'fixed',
                    'top'     : distanceFromTop,
                    'bottom'  : 'auto',
                    'left'    : parentLeftEdge - stickyWidth
                });
            } else {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'fixed',
                    'top'     : distanceFromTop,
                    'right'   : 'auto',
@@ -61,16 +79,16 @@ module.exports = function (sticky, distanceFromTop, right) {
                    'left'    : parentRightEdge
                });
            }
-       } else if (wST > bottomLimit - stickyHeight - distanceFromTop) { // Scroll position is on the bottom of the sticky parent
+       } else if (secondCondition) {
            if (!right) {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'absolute',
                    'top'     : 'auto',
                    'bottom'  : 0,
                    'left'    : -stickyWidth
                });
            } else {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'absolute',
                    'top'     : 'auto',
                    'right'   : 0,
@@ -78,15 +96,15 @@ module.exports = function (sticky, distanceFromTop, right) {
                    'left'    : parentWidth
                });
            }
-       } else { // Scroll is on top or above the sticky parent
+       } else { // Third condition - scroll position is on top or above the sticky parent
            if (!right) {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'absolute',
                    'top'     : 0,
                    'left' : '-60px'
                });
            } else {
-               $sticky.css({
+               $sticky.removeAttr('style').css({
                    'position': 'absolute',
                    'top'     : 0,
                    'right'   : 0,
@@ -96,5 +114,4 @@ module.exports = function (sticky, distanceFromTop, right) {
        }
     }
 
-    $window.on('load scroll resize', scrollItSticky);
 }
